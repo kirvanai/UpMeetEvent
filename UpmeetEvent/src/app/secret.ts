0005afdb4279fafb021d31ec0ec43f6906c9cbfc
Irving Kirvan <kirvanirving@gmail.com>
2023-11-30 16:44:00 -0500

    updating again

---
+++ UpmeetEvent/src/app/secret.ts
@@ -0,0 +1,5 @@
+export class Secret {
+    userUrl: string = "https://localhost:7090/api/User"
+    eventUrl: string = "https://localhost:7090/api/Events"
+    favoriteUrl: string = "https://localhost:7090/api/Favorite"
+}
