63503286765d01744e26c5d4ee28315b03ed7c5e
Alex Young <alexanderjyoung15@gmail.com>
2023-11-30 16:46:02 -0500

    Secret

---
+++ UpMeetEventBackend/UpMeetEventBackend/Secret.cs
@@ -0,0 +1,8 @@
+﻿namespace UpMeetEventBackend
+{
+    public class Secret
+    {
+        public static string ConnectionString { get; } = "Server=ALEX-2020BUILD;Database=Events;Trusted_Connection=True;TrustServerCertificate=True";
+
+    }
+}
