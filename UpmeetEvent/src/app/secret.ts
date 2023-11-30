c55a2e42d250533030d8e9a6aea2b7313f9d8920
Alex Young <alexanderjyoung15@gmail.com>
2023-11-30 16:44:26 -0500

    final update

---
+++ UpmeetEvent/src/app/secret.ts
@@ -0,0 +1,6 @@
+export class Secret {
+    userurl: string = "https://localhost:7090/api/User"
+    eventsurl: string ="https://localhost:7090/api/Events"
+    favoritesurl: string = "https://localhost:7090/api/Favorites"
+
+}
